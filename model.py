6b3d205792826c9cf5e430afcd9349f0bfe3723c
TatTanTai <87525948+TatTanTai@users.noreply.github.com>
2022-12-12 21:47:48 +0700

    Done

--- model.py
+++ model.py
@@ -31,44 +31,44 @@ class User(BaseModel, UserMixin):
     def __str__(self):
         return self.name
 
-class YTa(BaseModel):
-    __tablename__ = 'yta'
+# class YTa(BaseModel):
+#     __tablename__ = 'yta'
     
-    name = Column(String(70), nullable = True)
-    email = Column(String(100), nullable = True)
-    phone = Column(String(10), nullable = True)
+#     name = Column(String(70), nullable = True)
+#     email = Column(String(100), nullable = True)
+#     phone = Column(String(10), nullable = True)
     
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-class BacSi(BaseModel):
-    __tablename__ = 'bacsi'
+# class BacSi(BaseModel):
+#     __tablename__ = 'bacsi'
     
-    name = Column(String(70), nullable = True)
-    email = Column(String(100), nullable = True)
-    phone = Column(String(10), nullable = True)
-    def __str__(self):
-        return self.name
+#     name = Column(String(70), nullable = True)
+#     email = Column(String(100), nullable = True)
+#     phone = Column(String(10), nullable = True)
+#     def __str__(self):
+#         return self.name
 
-class ThuNgan(BaseModel):
-    __tablename__ = 'thungan'
+# class ThuNgan(BaseModel):
+#     __tablename__ = 'thungan'
     
-    name = Column(String(70), nullable = True)
-    email = Column(String(100), nullable = True)
-    phone = Column(String(10), nullable = True)
+#     name = Column(String(70), nullable = True)
+#     email = Column(String(100), nullable = True)
+#     phone = Column(String(10), nullable = True)
     
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-class QuanTri(BaseModel):
-    __tablename__ = 'quantri'
+# class QuanTri(BaseModel):
+#     __tablename__ = 'quantri'
     
-    name = Column(String(70), nullable = True)
-    email = Column(String(100), nullable = True)
-    phone = Column(String(10), nullable = True)
+#     name = Column(String(70), nullable = True)
+#     email = Column(String(100), nullable = True)
+#     phone = Column(String(10), nullable = True)
     
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 class NguoiBenh(BaseModel):
     __tablename__ = 'nguoibenh'
@@ -94,13 +94,9 @@ class Thuoc(BaseModel):
     def __str__(self):
         return self.name
 
-# prod_thuoc = db.Table('prod_thuoc',
-#                     Column('thuoc_id', Integer, ForeignKey('thuoc.id'), primary_key=True),
-#                     Column('phieukham_id', Integer, ForeignKey('phieukham.id'), primary_key=True))
 
 class DSKham(BaseModel):
     __tablename__ = 'dskham'
-    # nguoiBenh = relationship('NguoiBenh', backref = 'nguoibenh', lazy = True)
     name = Column(String(70), nullable = True)
     ngayDKKham = Column(String(10), nullable = True)
     address = Column(String(150), nullable = True)
@@ -123,13 +119,6 @@ class PhieuKham(BaseModel):
     def __str__(self):
         return self.id
 
-# class LichSuBenhNhan(BaseModel):
-#     id_PhieuKham = Column(Integer, ForeignKey('phieukham.id'))
-#     soLanTaiKham = Column(String(5))
-
-#     def __str__(self):
-#         return self.name
-
 class HoaDon(BaseModel):
     __tablename__ = 'hoadon'
     name = Column(String(70), nullable = False)
@@ -140,105 +129,12 @@ class HoaDon(BaseModel):
     ngayKham =  Column(String(10), nullable = False)
     id_User = Column(Integer, ForeignKey(User.id), nullable=False)
     id_PhieuKham = Column(Integer, ForeignKey(PhieuKham.id), nullable=False)
-    # phieukham = relationship('PhieuKham', backref='hoadon', lazy=True)
-    # doanhthu = relationship('DoanhThuThang', backref='hoadon', lazy=True)
     def __str__(self):
         return self.name
 
-# class DoanhThuThang(BaseModel):
-#     thang = Column(String(2), nullable = False)
-#     ngay = Column(String(10), nullable = False)
-#     soBenhNhan = Column(String(3), nullable = False)
-#     doanhThu = Column(String(20), nullable = False)
-#     tiLe = Column(String(3), nullable = False)
-#     id_HoaDon = Column(Integer,ForeignKey(HoaDon.id), nullable=False)
-#     def __str__(self):
-#         return self.name
-
-# class ThuocSDThang(BaseModel):
-#     id_Thuoc = Column(Integer, ForeignKey('thuoc.id'))
-#     soLuongSD = Column(String(10), nullable = True)
-
-#     def __str__(self):
-#         return self.name
-
 if __name__ == "__main__":
-#     with app.app_context():
-#             db.create_all()
-        
-        # password = str(hashlib.md5('123456'.encode('utf-8')).hexdigest())
-        # u = User(name='ThuNgan', username='ThuNgan', password=password,
-        #         user_role=UserRole.USER)
-        # db.session.add(u)
-        # db.session.commit()
     db.create_all()
 
-    # c1 = Category(name='Trang chủ')
-    # c2 = Category(name='Máy Tính Bảng')
-    # c3 = Category(name='Đồng Hồ Thông Minh')
-
-    # db.session.add(c1)
-    # db.session.add(c2)
-    # db.session.add(c3)
-
-    # db.session.commit()
-
-#     product = [{
-#     "id": 1,
-#     "name": "iPhone 7 Plus",
-#     "description": "Apple, 32GB, RAM: 3GB, iOS13",
-#     "price": 17000000,
-#     "image": "https://cdn.tgdd.vn/Products/Images/42/251192/iphone-14-pro-vang-1-2.jpg",
-#     "category_id": 1
-#    }, {
-#     "id": 2,
-#     "name": "iPad Pro 2020",
-#     "description": "Apple, 128GB, RAM: 6GB",
-#     "price": 37000000,
-#     "image": "https://cdn.tgdd.vn/Products/Images/42/251192/iphone-14-pro-vang-1-2.jpg",
-#     "category_id": 2
-#    }, {
-#     "id": 3,
-#     "name": "Galaxy Note 10 Plus",
-#    "description": "Samsung, 64GB, RAML: 6GB",
-#     "price": 24000000,
-#     "image": "https://cdn.tgdd.vn/Products/Images/42/251192/iphone-14-pro-vang-1-2.jpg",
-#     "category_id": 1
-#    }, {
-#     "id": 4,
-#     "name": "iPad Pro 2019",
-#     "description": "Apple, 128GB, RAM: 6GB",
-#     "price": 37000000,
-#     "image": "https://cdn.tgdd.vn/Products/Images/42/251192/iphone-14-pro-vang-1-2.jpg",
-#     "category_id": 2
-#    }, {
-#     "id": 5,
-#     "name": "Iphone 11",
-#     "description": "Apple, 128GB, RAM: 6GB",
-#     "price": 37000000,
-#     "image": "https://cdn.tgdd.vn/Products/Images/42/251192/iphone-14-pro-vang-1-2.jpg",
-#     "category_id": 2
-#    }
-#    , {
-#     "id": 5,
-#     "name": "Iphone 12",
-#     "description": "Apple, 128GB, RAM: 6GB",
-#     "price": 37000000,
-#     "image": "https://cdn.tgdd.vn/Products/Images/42/251192/iphone-14-pro-vang-1-2.jpg",
-#     "category_id": 2
-#    }
-# ]
-
-#     for p in product:
-#         pro = Product(name=p['name'], price=p['price'], image=p['image'], description=p['description'], category_id=p['category_id'])
-
-#         db.session.add(pro)
-
-
-
-
-
-#     db.session.commit()
     
 
 
